5fb0acdf320721eaea3bcf80661e0dcf7452b399
r03iuL <robiulislam1806@gmail.com>
2023-05-23 23:39:33 +0600

    About section added

---
+++ src/component/About/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="bg-gray-400 border-solid border-4 border-black rounded my-10 py-10 mx-20">
+      <div className="container  px-20 ">
+        <h2 className="text-3xl font-bold mb-4">About Us</h2>
+
+        <div>
+        <div>
+  <h2>Welcome to The Toy Store!</h2>
+  <p>Established in 1990, The Toy Store has been a trusted destination for toy enthusiasts and collectors for over three decades. With our long-standing commitment to quality and passion for all things toys, we have become renowned for providing an exceptional shopping experience.At The Toy Store, we believe in the power of play and its ability to inspire creativity, imagination, and joy. We take pride in curating a diverse and captivating selection of toys that cater to children of all ages, as well as the discerning tastes of collectors.</p>
+  <p>Our dedicated team of knowledgeable and friendly staff is here to assist you in finding the perfect toy. Whether you're searching for a beloved classic, the latest trend, or a unique collectible piece, we are committed to helping you discover something special.We understand the importance of safety and quality when it comes to toys. That's why we meticulously select our products from reputable manufacturers and suppliers who share our commitment to providing safe and reliable toys that meet the highest standards.</p>
+  <p>Thank you for choosing The Toy Store as your preferred destination for all things toys. We value your trust and are honored to be a part of your journey in bringing smiles and happiness to children and collectors alike. Join us and experience the wonder and excitement that The Toy Store has to offer!</p>
+</div>
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
